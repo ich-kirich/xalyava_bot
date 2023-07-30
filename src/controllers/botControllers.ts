ccac020d18a98d338931abbc47d307b1df683e70
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2023-07-30 16:24:24 +0300

    feat: implement message response

---
+++ src/controllers/botControllers.ts
@@ -0,0 +1,38 @@
+import ApiError from "../error/apiError";
+import { StatusCodes } from "http-status-codes";
+import TelegramBot from "node-telegram-bot-api";
+import { MESSAGES } from "../libs/constants";
+
+class BotControllers {
+  messagesToBot(bot: TelegramBot) {
+    try {
+      bot.on("message", (msg) => {
+        const message = msg.text;
+        const chatId = msg.chat.id;
+
+        switch (message) {
+          case "/start":
+            bot.sendMessage(chatId, MESSAGES.HELLO_MESSAGE, {
+              disable_web_page_preview: true,
+            });
+            break;
+          case "/startXalyava":
+            bot.sendMessage(chatId, MESSAGES.START_MAILING);
+            break;
+          case "/stopXalyava":
+            bot.sendMessage(chatId, MESSAGES.STOP_MAILING);
+            break;
+          default:
+            bot.sendMessage(chatId, MESSAGES.UNKNOWN_MEASSAGE);
+            break;
+        }
+      });
+    } catch (e) {
+      console.log(
+        new ApiError(e.status || StatusCodes.INTERNAL_SERVER_ERROR, e.message),
+      );
+    }
+  }
+}
+
+export default new BotControllers();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Start app");
+import TelegramBot from "node-telegram-bot-api";
+import config from "config";
+import BotControllers from "./controllers/botControllers";
+import { StatusCodes } from "http-status-codes";
+import ApiError from "./error/apiError";
+
+const startServer = async () => {
+  try {
+    const bot = new TelegramBot(config.get("telegram.apiKey"), {
+      polling: true,
+    });
+    BotControllers.messagesToBot(bot);
+    console.log("The bot is up and running");
+  } catch (e) {
+    console.log(
+      new ApiError(e.status || StatusCodes.INTERNAL_SERVER_ERROR, e.message),
+    );
+  }
+};
+
+startServer();
