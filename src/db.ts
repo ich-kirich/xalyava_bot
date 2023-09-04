e5299c36e0e13cc51d7a5f10bc54ea60ad292ef0
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2023-09-04 11:09:52 +0300

    fix: update deploy

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,6 @@
 import config from "config";
 import { Sequelize } from "sequelize";
 
-console.log(config.get("telegram.apiKey"));
-console.log(config.get("db.password"));
-
 const sequelize = new Sequelize(
   String(config.get("db.database")),
   String(config.get("db.username")),
