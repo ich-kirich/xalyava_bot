d2f158108fa60d564031dac7ea8408ba0efbadc0
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2023-09-17 17:53:38 +0300

    update

--- src/models/initDb.ts
+++ src/models/initDb.ts
@@ -1,20 +1,8 @@
 import logger from "../libs/logger";
 import sequelize from "../db";
-import config from "config";
-import ApiError from "../error/apiError";
 
 const initDb = async (): Promise<void> => {
-  console.log(config.get("db.database"));
-  console.log(config.get("db.username"));
-  console.log(config.get("db.password"));
-  console.log(config.get("db.dialect"));
-  console.log(config.get("db.host"));
-  console.log(config.get("db.port"));
-  try {
-    await sequelize.authenticate();
-  } catch(e) {
-    logger.error("Db error", new ApiError(e.status, e.message));
-  }
+  await sequelize.sync();
   logger.info("Database connection established successfully");
   return;
 };
